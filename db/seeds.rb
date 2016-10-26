8838586cf0e2a673567ce1a07067f2a1dec452aa
samdarbonne <samdarbonne@gmail.com>
2016-10-26 14:35:48 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.destroy_all
+Ingredient.destroy_all
+
+ingredient_list = [{
+										name: 'Yam'
+									},
+									{
+										name: 'Carrot'
+									},
+									{
+										name: 'Broth'
+									}]
+
+recipe_list = [{
+								name: 'Yam Stew',
+							},
+							{
+								name: 'Mashed Yams'
+							}]
+for ingredient in ingredient_list
+	Ingredient.create(ingredient)
+	puts "created ingredient: #{ingredient[:name]}"
+end
+
+for recipe in recipe_list
+	Recipe.create(recipe)
+	puts "created recipe: #{recipe[:name]}"
+end
+
+Recipe.first.ingredients << [Ingredient.first, Ingredient.third]
+
+
+Recipe.second.ingredients << [Ingredient.first, Ingredient.second]
