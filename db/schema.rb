ed101afab53483f580cb7f7439dc2753d29770d4
samdarbonne <samdarbonne@gmail.com>
2016-10-26 19:11:36 -0700

    adds Cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026201810) do
+ActiveRecord::Schema.define(version: 20161027020734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredient_lists", force: :cascade do |t|
     t.integer  "recipe_id"
