b93501812ebf962249abe990d27b5dcfd420e92f
samdarbonne <samdarbonne@gmail.com>
2016-10-31 13:46:24 -0700

    adds better fade animations and partials to browse index

--- app/assets/javascripts/browse.js
+++ app/assets/javascripts/browse.js
@@ -10,8 +10,9 @@ function tabClickListener(tabnum) {
 	$('#tab-' + tabnum).on('click', function(event) {
 		event.preventDefault();
 		$('.tab-group').addClass('hidden');
-		$('#tab-group-' + tabnum).removeClass('hidden animated fadeIn');
-		$('#tab-group-' + tabnum).addClass('animated fadeIn');
+		$('#tab-group-' + tabnum).removeClass('hidden');
+		$('#tab-group-' + tabnum).children().removeClass('animated fadeIn2')
+		$('#tab-group-' + tabnum).children().addClass('animated fadeIn2');
 		$('.tab').removeClass('active animated fadeIn');
 		$(this).addClass('active animated fadeIn');
 	})
