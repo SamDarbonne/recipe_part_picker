84711e07a90efed30b0873962f7a50e5c6af8165
samdarbonne <samdarbonne@gmail.com>
2016-11-02 11:55:04 -0700

    shuffles ingredients and recipes in browse controller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,7 +1,12 @@
 class BrowseController < ApplicationController
   def index
+  	# there's not enough chaos in the world, let's shuffle our ingredients and recipes!
+  	# this keeps things 'fresh'
   	@recipes = Recipe.all
+  	@recipes = @recipes.shuffle
   	@ingredients = Ingredient.all
+  	@ingredients = @ingredients.shuffle
+  	# chefs are a proud bunch and we will get complaints if we shuffle their order. 
   	@chefs = User.all.select do |user|
   		user.chef == true
   	end
